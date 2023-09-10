cd8a0fac71203d663958d65406fb8ce08c38fcf9
Ilya <shcherbakov.illya@list.ru>
2023-09-10 23:54:07 +0400

    skeleton

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,14 +4,19 @@ import styles from './styles.module.css'
 import { useState } from 'react';
 import { getNews } from '../../api/apiNews';
 import NewsList from '../../components/NewsList/NewsList';
+import Skeleton from '../../components/Skeleton/Skeleton';
 
 const Main = () => {
     const [news, setNews] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
     useEffect(() => {
         const fetchNews = async () => {
             try {
+              setIsLoading(true);
               const response = await getNews();
               setNews(response.news);
+              setIsLoading(false);
             } catch (error) {
                console.log(error); 
             }
@@ -19,9 +24,17 @@ const Main = () => {
         fetchNews();
     }, [])
    return (
-   <main className={styles.main}>
-    {news.length > 0 ? <NewsBanner item={news[0]} /> : null}
-    <NewsList news = {news}/>
+    <main className={styles.main}>
+        {news.length > 0 && !isLoading ? (
+            <NewsBanner item={news[0]} />
+        ) : (
+            <Skeleton type={'banner'} count={1}/>
+        )}
+        {!isLoading ? (
+            <NewsList news = {news}/>
+        ) : (
+            <Skeleton type={'item'} count={10}/>
+        )}
    </main>
    );
 };
